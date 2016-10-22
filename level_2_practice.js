0a459e99b413a4c3273e0e18f05d8e59000e1421
jrose27 <jrose@ggc.edu>
2016-10-21 21:46:49 -0400

    Fixed bubbles and added labels

--- level_2_practice.js
+++ level_2_practice.js
@@ -32,7 +32,9 @@ var mainState = {
     preload: function () {
         game.load.image('space_background_image', "assets/space_background.jpg");
         game.load.image('bubble_image', "assets/bubble256.png");
-    
+        
+        //Load JSON
+        game.load.text("chemicalFormula", "assets/game_data/level_2_data.json");
     },
 
     // function for adding pictures that are mostly static 
@@ -46,7 +48,7 @@ var mainState = {
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
-        this.scale.setScreenSize(true); 
+         
         
         // Create group of bubbles
         bubbles = game.add.group();
@@ -58,19 +60,40 @@ var mainState = {
         // Moves the image anchor to the middle, so it centers inside the game properly
         //bubbles.anchor.set(0.5);
         
+        bubble_labels = game.add.group();
+        bubble_labels.inputEnableChildren = true;
+        bubble_labels.enableBody = true;
+        bubble_labels.physicsBodyType = Phaser.Physics.ARCADE;
+        
         // Create 3 individual bubbles and allow them to be selected
-        bubble_01 = bubbles.create(width, 0, 'bubble_image');
+        bubble_01 = bubbles.create(width/4 - 220, 0, 'bubble_image');
         bubble_01.events.onInputDown.add(selectedBubble, this);
-        //bubble_text_01
-       
-        bubble_02 = bubbles.create(bubble_01.x + 275, 0, 'bubble_image');
+        // Create variable to hold the font style crap
+        var style = { font: "42px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: bubble_01.width, align: "center", backgroundColor: "rgba(0,0,0,0)" };
+        bubble_text_01 = game.add.text(0,0, "Bubble 01", style, bubble_labels);
+        bubble_text_01.anchor.set(0.5);
+        bubble_text_01.events.onInputDown.add(selectedBubble, this);
+        
+        bubble_02 = bubbles.create(bubble_01.x + 325, 0, 'bubble_image');
         bubble_02.events.onInputDown.add(selectedBubble, this);    
-    //  bubble_text_02
-       
-        bubble_03 = bubbles.create(bubble_02.x + 275, 0, 'bubble_image');
+        var style = { font: "42px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: bubble_02.width, align: "center", backgroundColor: "rgba(0,0,0,0)" };
+        bubble_text_02 = game.add.text(0,0, "Bubble 02", style, bubble_labels);
+        bubble_text_02.anchor.set(0.5);
+        bubble_text_02.events.onInputDown.add(selectedBubble, this);
+        
+        bubble_03 = bubbles.create(bubble_02.x + 325, 0, 'bubble_image');
         bubble_03.events.onInputDown.add(selectedBubble, this);
-    //  bubble_text_03
-  
+        var style = { font: "42px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: bubble_03.width, align: "center", backgroundColor: "rgba(0,0,0,0)" };
+        bubble_text_03 = game.add.text(0,0, "Bubble 03", style, bubble_labels);
+        bubble_text_03.anchor.set(0.5);
+        bubble_text_03.events.onInputDown.add(selectedBubble, this);
+        
+        
+        var tween = game.add.tween(bubbles).to({x: 200, y: innerHeight - bubbles.height}, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+        tween.onLoop.add(descend,this);
+        
+        var tween = game.add.tween(bubble_labels).to({x: 200, y: innerHeight - bubbles.height}, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+        tween.onLoop.add(descend,this);
          /* for (var i = 0; i < 3; i++) {
             var bubble = bubbles.create(300 + (300 * i), 0, 'bubble_image');
             bubble.events.onInputDown.add(selectedBubble, this);
@@ -82,6 +105,15 @@ var mainState = {
     update: function () {
         background.tilePosition.y += background_velocity;
         
+        // Update text label positions
+        bubble_text_01.x = Math.floor(bubble_01.x + bubble_01.width/2);
+        bubble_text_01.y = Math.floor(bubble_01.y + bubble_01.height/2);
+        
+        bubble_text_02.x = Math.floor(bubble_02.x + bubble_02.width/2);
+        bubble_text_02.y = Math.floor(bubble_02.y + bubble_02.height/2);
+        
+        bubble_text_03.x = Math.floor(bubble_03.x + bubble_03.width/2);
+        bubble_text_03.y = Math.floor(bubble_03.y + bubble_03.height/2); 
        /* if (game.input.activePointer.isDown == true) {
            background_velocity = 25;
         } else {
@@ -98,10 +130,14 @@ var mainState = {
 };
 
 function selectedBubble(input_bubble) {
-  //input_bubble.kill();
-  //input_bubble_text.kill();
-    input_bubble.y += 15;
     
+    input_bubble.kill();
+    //input_bubble.y += 15;
+    
+}
+
+function descend() {
+    bubbles.y += 10;
 }
 
 game.state.add('mainState', mainState);
